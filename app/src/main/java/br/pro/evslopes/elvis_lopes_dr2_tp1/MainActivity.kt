692af2ad3fda6c6fb0411e27b01adcbbe72c36ae
Elvis Lopes <elvis.lopes@al.infnet.edu.br>
2020-12-01 20:08:01 -0300

    TP1_DR1_ELVIS_LOPES - Calculadoras

---
+++ app/src/main/java/br/pro/evslopes/elvis_lopes_dr2_tp1/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.pro.evslopes.elvis_lopes_dr2_tp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
